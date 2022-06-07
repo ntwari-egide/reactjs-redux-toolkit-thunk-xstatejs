1f729c30308a0a7b90ea5be1e5462fd03cb0c1da
ntwari-egide <ntwariegide2@gmail.com>
2022-06-07 21:07:23 +0200

    ft install xstate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { userAdded } from './redux/usersSlice'
+import { editUser, removeUser, userAdded } from './redux/usersSlice'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,20 +14,31 @@ function App() {
   const dispatch = useDispatch();
 
   const handleAddUser = () => {
-    console.log('reached here 1 ....');
-    
-    const newUser = { username: 'giselle', age: 34}
+ 
+    const newUser = { id: 1, username: 'giselle', age: 34}
 
     dispatch(
       userAdded(newUser)
     )
     
   }
+
+  const handleEditUser = () => {
+    const updates = { id: 1, username: 'giselle', age: 34}
+
+    dispatch(
+      editUser(updates)
+    )
+  }
+
+  const handleDeleteUser = () => dispatch(removeUser({id: 1}))
   
 
   return (
     <div className="App">
       <button onClick={handleAddUser}>ADD NEW USER</button>
+      <button onClick={handleEditUser}>EDIT NEW USER</button>
+      <button onClick={handleDeleteUser}>DELETE USER</button>
     </div>
   )
 }
--- src/redux/usersSlice.tsx
+++ src/redux/usersSlice.tsx
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {username: 'ntwari', age: 21},
-    {username: 'kagabo', age: 43},
-    {username: 'mugisha', age: 34}
+    {id: 1, username: 'ntwari', age: 21},
+    {id: 2, username: 'kagabo', age: 43},
+    {id: 3,username: 'mugisha', age: 34}
 ]
 
 const usersSlice = createSlice({
@@ -15,11 +15,22 @@ const usersSlice = createSlice({
             console.log('reached ...');
             
             state.push(action.payload)
+        },
+        editUser( state, action ) {
+            let index = state.findIndex( user => user.id === action.payload.id )
+            state[index].username = action.payload.username
+            state[index].age = action.payload.age
+            return state
+        },
+        removeUser(state, action ){
+            let index = state.findIndex(user => user.id === action.payload.id)
+            state.splice(index,1)
+            return state
         }
     }
 })
 
 // making action available
-export const {userAdded} = usersSlice.actions
+export const {userAdded, editUser, removeUser} = usersSlice.actions
 
 export default usersSlice.reducer
